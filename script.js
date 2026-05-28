9f144fe3bda52e9a23a632e45eeb97e0fb803a42
iknoor12 <imiknoor@gmail.com>
2026-05-28 09:31:45 +0530

    frontend assessment

---
+++ script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".heroSwiper", {
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
